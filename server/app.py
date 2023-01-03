38073d6a6c9a398c1f27e5ec002bbd123a54e431
Diego <diegogplata@gmail.com>
2023-01-03 13:19:18 -0600

    append lmb routes in the form of include_router method

--- server/app.py
+++ server/app.py
@@ -1,31 +1,12 @@
 from fastapi import FastAPI, APIRouter, Body
 from fastapi.encoders import jsonable_encoder
-from server.database import retrieve_players_hitting_stats
-
+from server.routes.lmb import router as lmb_router
 
 app = FastAPI()
 
 # test motor vs pymongo
-
-def ResponseModel(data, message):
-    return {
-        "data": data,
-        "code": 200,
-        "message": message,
-    }
+app.include_router(lmb_router, tags=["lmb"], prefix="/lmb")
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
-
-
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
-
-@app.get("/players", response_description="players retrieved")
-async def get_players_hitting_stats():
-    hitting_stats = await retrieve_players_hitting_stats()
-    if hitting_stats:
-        return ResponseModel(hitting_stats, "Players data retrieved successfully")
-    return ResponseModel(hitting_stats, "Empty list returned")
