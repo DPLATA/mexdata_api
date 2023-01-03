c58b744ed4b41d6b44361e6d5c2f1ebf4efb013d
Diego <diegogplata@gmail.com>
2023-01-02 18:16:16 -0600

    add model for successful response, add players GET route

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, APIRouter, Body
+from fastapi.encoders import jsonable_encoder
+from database import retrieve_players_hitting_stats
+
 
 app = FastAPI()
 
+# test motor vs pymongo
+
+def ResponseModel(data, message):
+    return {
+        "data": data,
+        "code": 200,
+        "message": message,
+    }
 
 @app.get("/")
 async def root():
@@ -11,3 +22,10 @@ async def root():
 @app.get("/hello/{name}")
 async def say_hello(name: str):
     return {"message": f"Hello {name}"}
+
+@app.get("/players", response_description="players retrieved")
+async def get_players_hitting_stats():
+    hitting_stats = await retrieve_players_hitting_stats()
+    if hitting_stats:
+        return ResponseModel(hitting_stats, "Players data retrieved successfully")
+    return ResponseModel(hitting_stats, "Empty list returned")
