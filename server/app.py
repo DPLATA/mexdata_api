b0a39e89557c162d17724de7cc0ce25a806fee44
Diego <diegogplata@gmail.com>
2023-01-03 15:12:18 -0600

    add CORS for accessing API from outside resources

--- server/app.py
+++ server/app.py
@@ -1,8 +1,19 @@
 from fastapi import FastAPI, APIRouter, Body
-from fastapi.encoders import jsonable_encoder
+# from fastapi.encoders import jsonable_encoder
+from fastapi.middleware.cors import CORSMiddleware
 from server.routes.lmb import router as lmb_router
 
 app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # test motor vs pymongo
 app.include_router(lmb_router, tags=["lmb"], prefix="/lmb")
