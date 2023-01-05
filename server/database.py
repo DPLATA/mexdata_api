aa6f166b797ff103e113c02bf469eea709a83df5
Diego <diegogplata@gmail.com>
2023-01-04 21:32:50 -0600

    introduce necessary changes to include features collections route as part of mex_map router

--- server/database.py
+++ server/database.py
@@ -7,86 +7,103 @@ db = client[config('MONGO_DB')]
 player_hitting_collection = db.get_collection(config('MONGO_PLAYER_HITTING_COLLECTION'))
 team_hitting_collection = db.get_collection(config('MONGO_TEAM_HITTING_COLLECTION'))
 
-feature_collection = db.get_collection(config('MONGO_TEAM_MEX_MAP_FEATURE_COLLECTION'))
+single_features_collection = db.get_collection(config('MONGO_TEAM_MEX_MAP_SINGLE_FEATURES_COLLECTION'))
+feature_collection_collection = db.get_collection(config('MONGO_TEAM_MEX_MAP_FEATURE_COLLECTION_COLLECTION'))
 
 
 # helpers
-def player_ocurrence_helper(ocurrence) -> dict:
+def player_occurrence_helper(occurrence) -> dict:
     return {
-        "id": str(ocurrence["_id"]),
-        "player": ocurrence["player"],
-        "team": ocurrence["team"],
-        "G": ocurrence["G"],
-        "AB": ocurrence["AB"],
-        "R": ocurrence["R"],
-        "H": ocurrence["H"],
-        "2B": ocurrence["2B"],
-        "3B": ocurrence["3B"],
-        "HR": ocurrence["HR"],
-        "RBI": ocurrence["RBI"],
-        "BB": ocurrence["BB"],
-        "SO": ocurrence["SO"],
-        "SB": ocurrence["SB"],
-        "CS": ocurrence["CS"],
-        "AVG": ocurrence["AVG"],
-        "OBP": ocurrence["OBP"],
-        "SLG": ocurrence["SLG"],
-        "OPS": ocurrence["OPS"]
+        "id": str(occurrence["_id"]),
+        "player": occurrence["player"],
+        "team": occurrence["team"],
+        "G": occurrence["G"],
+        "AB": occurrence["AB"],
+        "R": occurrence["R"],
+        "H": occurrence["H"],
+        "2B": occurrence["2B"],
+        "3B": occurrence["3B"],
+        "HR": occurrence["HR"],
+        "RBI": occurrence["RBI"],
+        "BB": occurrence["BB"],
+        "SO": occurrence["SO"],
+        "SB": occurrence["SB"],
+        "CS": occurrence["CS"],
+        "AVG": occurrence["AVG"],
+        "OBP": occurrence["OBP"],
+        "SLG": occurrence["SLG"],
+        "OPS": occurrence["OPS"]
     }
 
 
-def team_ocurrence_helper(ocurrence) -> dict:
+def team_occurrence_helper(occurrence) -> dict:
     return {
-        "id": str(ocurrence["_id"]),
-        "team": ocurrence["team"],
-        "league": ocurrence["league"],
-        "G": ocurrence["G"],
-        "AB": ocurrence["AB"],
-        "R": ocurrence["R"],
-        "H": ocurrence["H"],
-        "2B": ocurrence["2B"],
-        "3B": ocurrence["3B"],
-        "HR": ocurrence["HR"],
-        "RBI": ocurrence["RBI"],
-        "BB": ocurrence["BB"],
-        "SO": ocurrence["SO"],
-        "SB": ocurrence["SB"],
-        "CS": ocurrence["CS"],
-        "AVG": ocurrence["AVG"],
-        "OBP": ocurrence["OBP"],
-        "SLG": ocurrence["SLG"],
-        "OPS": ocurrence["OPS"]
+        "id": str(occurrence["_id"]),
+        "team": occurrence["team"],
+        "league": occurrence["league"],
+        "G": occurrence["G"],
+        "AB": occurrence["AB"],
+        "R": occurrence["R"],
+        "H": occurrence["H"],
+        "2B": occurrence["2B"],
+        "3B": occurrence["3B"],
+        "HR": occurrence["HR"],
+        "RBI": occurrence["RBI"],
+        "BB": occurrence["BB"],
+        "SO": occurrence["SO"],
+        "SB": occurrence["SB"],
+        "CS": occurrence["CS"],
+        "AVG": occurrence["AVG"],
+        "OBP": occurrence["OBP"],
+        "SLG": occurrence["SLG"],
+        "OPS": occurrence["OPS"]
     }
 
 
-def feature_ocurrence_helper(ocurrence) -> dict:
+def single_features_occurrence_helper(occurrence) -> dict:
     return {
-        "id": str(ocurrence["_id"]),
-        "type": ocurrence["type"],
-        "properties": ocurrence["properties"],
-        "geometry": ocurrence["geometry"]
+        "id": str(occurrence["_id"]),
+        "type": occurrence["type"],
+        "properties": occurrence["properties"],
+        "geometry": occurrence["geometry"]
     }
 
 
-# Retrieve all players hitting stats present in the database
+def feature_collection_occurrence_helper(occurrence) -> dict:
+    return {
+        "id": str(occurrence["_id"]),
+        "type": occurrence["type"],
+        "features": occurrence["features"]
+    }
+
+
+# Retrieve hitting stats for all players present in the database
 async def retrieve_players_hitting_stats():
     players = []
     async for player in player_hitting_collection.find():
-        players.append(player_ocurrence_helper(player))
+        players.append(player_occurrence_helper(player))
     return players
 
 
-# Retrieve all teams hitting stats present in the database
+# Retrieve hitting stats for all teams present in the database
 async def retrieve_teams_hitting_stats():
     teams = []
     async for team in team_hitting_collection.find():
-        teams.append(team_ocurrence_helper(team))
+        teams.append(team_occurrence_helper(team))
     return teams
 
 
-# Retrieve all geoJson features present in the database
-async def retrieve_geojson_feature():
-    geojson_features = []
-    async for feature in feature_collection.find():
-        geojson_features.append(feature_ocurrence_helper(feature))
-    return geojson_features
+# Retrieve all geoJson single features present in the database
+async def retrieve_geojson_all_single_features():
+    geojson_single_features = []
+    async for feature in single_features_collection.find():
+        geojson_single_features.append(single_features_occurrence_helper(feature))
+    return geojson_single_features
+
+
+# Retrieve all geoJson feature collections present in the database
+async def retrieve_geojson_all_feature_collections():
+    geojson_feature_collections = []
+    async for feature_collection in feature_collection_collection.find():
+        geojson_feature_collections.append(feature_collection_occurrence_helper(feature_collection))
+    return geojson_feature_collections
--- server/routes/mex_map.py
+++ server/routes/mex_map.py
@@ -2,7 +2,8 @@ from fastapi import APIRouter, Body
 from fastapi.encoders import jsonable_encoder
 
 from server.database import (
-    retrieve_geojson_feature
+    retrieve_geojson_all_feature_collections,
+    retrieve_geojson_all_single_features
 )
 
 from server.models.response import (
@@ -13,9 +14,17 @@ from server.models.response import (
 router = APIRouter()
 
 
-@router.get("/features", response_description="geoJson features retrieved")
-async def get_geojson_mex_map():
-    mex_geojson_feature_collection = await retrieve_geojson_feature()
-    if mex_geojson_feature_collection:
-        return successful_response_model(mex_geojson_feature_collection, "geoJson features retrieved successfully")
-    return successful_response_model(mex_geojson_feature_collection, "empty list returned")
+@router.get("/single_features", response_description="geoJson single features retrieved")
+async def get_geojson_mex_map_as_single_features():
+    mex_geojson_single_features = await retrieve_geojson_all_single_features()
+    if mex_geojson_single_features:
+        return successful_response_model(mex_geojson_single_features, "geoJson single features retrieved successfully")
+    return successful_response_model(mex_geojson_single_features, "empty list returned")
+
+
+@router.get("/feature_collections", response_description="geoJson feature collections retrieved")
+async def get_geojson_mex_map_as_feature_collection():
+    mex_geojson_feature_collections = await retrieve_geojson_all_feature_collections()
+    if mex_geojson_feature_collections:
+        return successful_response_model(mex_geojson_feature_collections, "geoJson feature collections retrieved successfully")
+    return successful_response_model(mex_geojson_feature_collections, "empty list returned")
